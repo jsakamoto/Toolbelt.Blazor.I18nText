7caf6366ec98fddee7b00584f8786f75fa4a2f4d
jsakamoto <junichi.jp@gmail.com>
2019-01-06 15:19:15 +0900

    Organize sample programs.

---
+++ SampleSites/ClientSideSampleSite/Startup.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace ClientSideSampleSite
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+
+            services.AddI18nText<Startup>();
+        }
+
+        public void Configure(IBlazorApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
---
+++ SampleSites/ServerSideSampleSite/ServerSideSampleSite.App/i18ntext/@types/ServerSideSampleSite.App.I18nText.Text.cs
@@ -0,0 +1,20 @@
+namespace ServerSideSampleSite.App.I18nText
+{
+    public class Text
+    {
+        /// <summary>Click me</summary>
+        public string ClickMe;
+
+        /// <summary>Counter</summary>
+        public string Counter;
+
+        /// <summary>Fetch data</summary>
+        public string FetchData;
+
+        /// <summary>Hello, world!</summary>
+        public string HelloWorld;
+
+        /// <summary>Home</summary>
+        public string Home;
+    }
+}
