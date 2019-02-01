4c5f943d00dd9c478f96ef9cf4e0ec699dc68ea0
jsakamoto <junichi.jp@gmail.com>
2019-02-01 23:06:41 +0900

    Upgrade package references.

--- Tests/Lib4PackRef/i18ntext/@types/Lib4PackRef.I18nText.Text.cs
+++ Tests/Lib4PackRef/i18ntext/@types/Lib4PackRef.I18nText.Text.cs
@@ -1,8 +1,10 @@
 namespace Lib4PackRef.I18nText
 {
-    public partial class Text : global::Toolbelt.Blazor.I18nText.I18nTextFallbackLanguage
+    public partial class Text : global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage, global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextLateBinding
     {
-        string global::Toolbelt.Blazor.I18nText.I18nTextFallbackLanguage.FallBackLanguage => "en";
+        string global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage.FallBackLanguage => "en";
+
+        public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
 
         /// <summary>"This Blazor component is defined in the Lib4PackRef package."</summary>
         public string Message;
--- Tests/Lib4ProjRef/i18ntext/@types/Lib4ProjRef.I18nText.Text.cs
+++ Tests/Lib4ProjRef/i18ntext/@types/Lib4ProjRef.I18nText.Text.cs
@@ -1,8 +1,10 @@
 namespace Lib4ProjRef.I18nText
 {
-    public partial class Text : global::Toolbelt.Blazor.I18nText.I18nTextFallbackLanguage
+    public partial class Text : global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage, global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextLateBinding
     {
-        string global::Toolbelt.Blazor.I18nText.I18nTextFallbackLanguage.FallBackLanguage => "ja";
+        string global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage.FallBackLanguage => "ja";
+
+        public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
 
         /// <summary>"この Blazor コンポーネントは Lib4ProjRef で定義されています。"</summary>
         public string Message;
