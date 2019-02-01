16e055d50136c1d660add4665ac0c5c9a77492bb
jsakamoto <junichi.jp@gmail.com>
2019-02-01 23:09:23 +0900

    Localize Stand Alone App with 'en' and 'ja'.

---
+++ Tests/StandAloneApp/StandAloneApp/i18ntext/@types/StandAloneApp.I18nText.Text.cs
@@ -0,0 +1,75 @@
+namespace StandAloneApp.I18nText
+{
+    public partial class Text : global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage, global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextLateBinding
+    {
+        string global::Toolbelt.Blazor.I18nText.Interfaces.I18nTextFallbackLanguage.FallBackLanguage => "en";
+
+        public string this[string key] => global::Toolbelt.Blazor.I18nText.I18nTextExtensions.GetFieldValue(this, key);
+
+        /// <summary>"About"</summary>
+        public string About;
+
+        /// <summary>"Balmy"</summary>
+        public string Balmy;
+
+        /// <summary>"Bracing"</summary>
+        public string Bracing;
+
+        /// <summary>"Chilly"</summary>
+        public string Chilly;
+
+        /// <summary>"Click me"</summary>
+        public string ClickMe;
+
+        /// <summary>"Counter"</summary>
+        public string Counter;
+
+        /// <summary>"Current count"</summary>
+        public string CurrentCount;
+
+        /// <summary>"Date"</summary>
+        public string Date;
+
+        /// <summary>"Fetch data"</summary>
+        public string FetchData;
+
+        /// <summary>"Freezing"</summary>
+        public string Freezing;
+
+        /// <summary>"Hello, world!!"</summary>
+        public string HelloWorld;
+
+        /// <summary>"Home"</summary>
+        public string Home;
+
+        /// <summary>"Loading..."</summary>
+        public string Loading;
+
+        /// <summary>"Stand Alone App"</summary>
+        public string StandAloneApp;
+
+        /// <summary>"Summary"</summary>
+        public string Summary;
+
+        /// <summary>"Please take our [brief survey]({0}) and tell us what you think."</summary>
+        public string SurveyPrompt;
+
+        /// <summary>"How is Blazor working for you?"</summary>
+        public string SurveyTitle;
+
+        /// <summary>"Temp. (C)"</summary>
+        public string TempC;
+
+        /// <summary>"Temp. (F)"</summary>
+        public string TempF;
+
+        /// <summary>"Weather forecast"</summary>
+        public string WeatherForecast;
+
+        /// <summary>"This component demonstrates fetching data from the server."</summary>
+        public string WeatherForecast_Description;
+
+        /// <summary>"Welcome to your new app."</summary>
+        public string WelcomeToYourNewApp;
+    }
+}
