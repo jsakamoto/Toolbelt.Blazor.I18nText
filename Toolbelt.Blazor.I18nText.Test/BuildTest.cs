c79681f02b736d7f975768b6f955310c2cddacc7
jsakamoto <junichi.jp@gmail.com>
2019-08-18 21:00:22 +0900

    Update unit test for building and publishing

--- Toolbelt.Blazor.I18nText.Test/BuildTest.cs
+++ Toolbelt.Blazor.I18nText.Test/BuildTest.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using Xunit;
 
-using static Toolbelt.Blazor.I18nText.Test.Shell;
+using static Toolbelt.Blazor.I18nText.Test.Internals.Shell;
 
 namespace Toolbelt.Blazor.I18nText.Test
 {
@@ -15,6 +14,18 @@ namespace Toolbelt.Blazor.I18nText.Test
         public BuildTest()
         {
             UpdateLibraryProjects();
+        }
+
+        private static HashSet<string> DirOfUpdatedProj = new HashSet<string>();
+
+        private static bool CheckAndMarkUpdated(string projectDir)
+        {
+            lock (DirOfUpdatedProj)
+            {
+                if (DirOfUpdatedProj.Contains(projectDir)) return true;
+                DirOfUpdatedProj.Add(projectDir);
+                return false;
+            }
         }
 
         private static void UpdateLibraryProjects()
@@ -27,8 +38,10 @@ namespace Toolbelt.Blazor.I18nText.Test
                 var libProjDocBefore = XDocument.Load(libProjPath);
                 var i18textVersionBefore = libProjDocBefore.Descendants("PackageReference").Where(node => node.Attribute("Include").Value == "Toolbelt.Blazor.I18nText").First().Attribute("Version").Value;
 
-                Run(libDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText");
-                ErrorLevel.Is(0);
+                if (CheckAndMarkUpdated(libDir)) continue;
+
+                Run(libDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText")
+                    .ExitCode.Is(0);
 
                 var libProjDocAfter = XDocument.Load(libProjPath);
                 var i18textVersionAfter = libProjDocAfter.Descendants("PackageReference").Where(node => node.Attribute("Include").Value == "Toolbelt.Blazor.I18nText").First().Attribute("Version").Value;
@@ -41,8 +54,7 @@ namespace Toolbelt.Blazor.I18nText.Test
                     libProjDocAfter.Save(libProjPath);
                 }
 
-                Run(libDir, "dotnet", "build");
-                ErrorLevel.Is(0);
+                Run(libDir, "dotnet", "build").ExitCode.Is(0);
             }
         }
 
@@ -54,97 +66,96 @@ namespace Toolbelt.Blazor.I18nText.Test
             return testDir;
         }
 
+        private static (string StartupProj, string ClientProj, string Bin, string Obj) GetDirectories(string startupProjDir, string clientProjDir)
+        {
+            var testDir = GetTestDir();
+            startupProjDir = Path.Combine(testDir, startupProjDir);
+            clientProjDir = Path.Combine(testDir, clientProjDir);
+            var binDir = Path.Combine(startupProjDir, "bin");
+            var objDir = Path.Combine(startupProjDir, "obj");
+
+            return (
+                StartupProj: startupProjDir,
+                ClientProj: clientProjDir,
+                Bin: binDir,
+                Obj: objDir);
+        }
+
         public static IEnumerable<object[]> Projects = new[] {
-            new object[]{"StandAloneApp", "StandAloneApp",        "netstandard2.0", "StandAloneApp"},
-            new object[]{"HostedApp",     "HostedApp.Server",     "netcoreapp3.0",  "HostedApp.Client"},
-            // TODO: Currently ASP.NET Core Razor Component (a.k.a Server Side Blazor) doesn't support Blazor library feature.
+            new object[]{ "Client", "netstandard2.0", "Client"},
+            new object[]{ "Server", "netcoreapp3.0", "Client"},
+            // TODO: Currently Server Side Blazor doesn't support Blazor library feature.
             // new object[]{"ServerSideApp", "ServerSideApp.Server", "netcoreapp2.1",  "ServerSideApp.App"},
         };
 
         [Theory(DisplayName = "Build")]
         [MemberData(nameof(Projects))]
-        public void BasicBuildTest(string solutionName, string stratupProjName, string platform, string clientProjName)
+        public void BasicBuildTest(string startupProjDir, string platform, string clientProjDir)
         {
-            var testDir = GetTestDir();
-            var startupProjDir = Path.Combine(testDir, solutionName, stratupProjName);
-            var clientProjDir = Path.Combine(testDir, solutionName, clientProjName);
+            var dirs = GetDirectories(startupProjDir, clientProjDir);
+            var distDir = Path.Combine(dirs.Bin, Path.Combine($"Debug/{platform}/dist/_content/i18ntext".Split('/')));
 
-            var binDir = Path.Combine(startupProjDir, "bin");
-            var objDir = Path.Combine(startupProjDir, "obj");
-            var distDir = Path.Combine(binDir, Path.Combine($"Debug/{platform}/dist/_content/i18ntext".Split('/')));
+            Delete(dirs.Bin);
+            Delete(dirs.Obj);
 
-            Delete(binDir);
-            Delete(objDir);
+            UpdatePackageRef(dirs.ClientProj);
 
-            Run(clientProjDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText");
-            ErrorLevel.Is(0);
-            Run(clientProjDir, "dotnet", "add", "package", "Lib4PackRef");
-            ErrorLevel.Is(0);
-            Run(testDir, "dotnet", "build", $"{solutionName}.sln");
-            ErrorLevel.Is(0);
+            Run(dirs.StartupProj, "dotnet", "build")
+                .ExitCode.Is(0);
 
-            Exists(distDir, "Lib4PackRef.I18nText.Text.en.json").IsTrue();
-            Exists(distDir, "Lib4PackRef.I18nText.Text.ja.json").IsTrue();
-            Exists(distDir, "Lib4ProjRef.I18nText.Text.en.json").IsTrue();
-            Exists(distDir, "Lib4ProjRef.I18nText.Text.ja.json").IsTrue();
+            var textResJsonFileNames = Directory.GetFiles(distDir, "*.*")
+                .Select(path => Path.GetFileName(path))
+                .OrderBy(name => name);
+
+            textResJsonFileNames.Is(
+                "Lib4PackRef.I18nText.Text.en.json",
+                "Lib4PackRef.I18nText.Text.ja.json",
+                "Lib4ProjRef.I18nText.Text.en.json",
+                "Lib4ProjRef.I18nText.Text.ja.json",
+                "SampleSite.Client.I18nText.Text.en.json",
+                "SampleSite.Client.I18nText.Text.ja.json"
+            );
         }
 
         [Theory(DisplayName = "Publish")]
         [MemberData(nameof(Projects))]
-        public void PublishTest(string solutionName, string stratupProjName, string platform, string clientProjName)
+        public void PublishTest(string startupProjDir, string platform, string clientProjDir)
         {
-            var testDir = GetTestDir();
-            var startupProjDir = Path.Combine(testDir, solutionName, stratupProjName);
-            var clientProjDir = Path.Combine(testDir, solutionName, clientProjName);
+            var dirs = GetDirectories(startupProjDir, clientProjDir);
+            var clientProjName = Path.GetFileNameWithoutExtension(Directory.GetFiles(dirs.ClientProj, "*.csproj").First());
+            var distDir = Path.Combine(dirs.Bin, Path.Combine($"Debug/{platform}/publish/{clientProjName}/dist/_content/i18ntext".Split('/')));
 
-            var binDir = Path.Combine(startupProjDir, "bin");
-            var objDir = Path.Combine(startupProjDir, "obj");
-            var distDir = Path.Combine(binDir, Path.Combine($"Debug/{platform}/publish/{clientProjName}/dist/content/i18ntext".Split('/')));
+            Delete(dirs.Bin);
+            Delete(dirs.Obj);
 
-            Delete(binDir);
-            Delete(objDir);
+            UpdatePackageRef(dirs.ClientProj);
 
-            Run(clientProjDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText");
-            ErrorLevel.Is(0);
-            Run(clientProjDir, "dotnet", "add", "package", "Lib4PackRef");
-            ErrorLevel.Is(0);
-            Run(testDir, "dotnet", "publish", $"{solutionName}.sln");
-            ErrorLevel.Is(0);
+            Run(dirs.StartupProj, "dotnet", "publish")
+                .ExitCode.Is(0);
 
-            Exists(distDir, "Lib4PackRef.I18nText.Text.en.json").IsTrue();
-            Exists(distDir, "Lib4PackRef.I18nText.Text.ja.json").IsTrue();
-            Exists(distDir, "Lib4ProjRef.I18nText.Text.en.json").IsTrue();
-            Exists(distDir, "Lib4ProjRef.I18nText.Text.ja.json").IsTrue();
-        }
-    }
+            var textResJsonFileNames = Directory.GetFiles(distDir, "*.*")
+                .Select(path => Path.GetFileName(path))
+                .OrderBy(name => name);
 
-    public static class Shell
-    {
-        public static int ErrorLevel { get; set; }
-
-        public static bool Exists(string dir, string wildCard)
-        {
-            return Directory.GetFiles(dir, wildCard, SearchOption.TopDirectoryOnly).Any();
+            textResJsonFileNames.Is(
+                "Lib4PackRef.I18nText.Text.en.json",
+                "Lib4PackRef.I18nText.Text.ja.json",
+                "Lib4ProjRef.I18nText.Text.en.json",
+                "Lib4ProjRef.I18nText.Text.ja.json",
+                "SampleSite.Client.I18nText.Text.en.json",
+                "SampleSite.Client.I18nText.Text.ja.json"
+            );
         }
 
-        public static void Delete(string dir)
+        private static void UpdatePackageRef(string projectDir)
         {
-            if (Directory.Exists(dir)) Directory.Delete(dir, recursive: true);
-        }
-
-        public static int Run(string workDir, params string[] args)
-        {
-            var pi = new ProcessStartInfo
+            if (!CheckAndMarkUpdated(projectDir))
             {
-                WorkingDirectory = workDir,
-                FileName = args.First(),
-                Arguments = string.Join(" ", args.Skip(1)),
-                UseShellExecute = false,
-            };
-            var process = Process.Start(pi);
-            process.WaitForExit();
-            ErrorLevel = process.ExitCode;
-            return process.ExitCode;
+                Run(projectDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText")
+                    .ExitCode.Is(0);
+                Run(projectDir, "dotnet", "add", "package", "Lib4PackRef")
+                    .ExitCode.Is(0);
+            }
         }
     }
 }
---
+++ Toolbelt.Blazor.I18nText.Test/Internals/Shell.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Toolbelt.Blazor.I18nText.Test.Internals
+{
+    public static class Shell
+    {
+        public static int ErrorLevel { get; set; }
+
+        public static bool Exists(string dir, string wildCard)
+        {
+            return Directory.GetFiles(dir, wildCard, SearchOption.TopDirectoryOnly).Any();
+        }
+
+        public static void Delete(string dir)
+        {
+            if (Directory.Exists(dir)) Directory.Delete(dir, recursive: true);
+        }
+
+        public static Process Run(string workDir, params string[] args)
+        {
+            var pi = new ProcessStartInfo
+            {
+                WorkingDirectory = workDir,
+                FileName = args.First(),
+                Arguments = string.Join(" ", args.Skip(1)),
+                UseShellExecute = false,
+            };
+            var process = Process.Start(pi);
+            process.WaitForExit();
+            ErrorLevel = process.ExitCode;
+            return process;
+        }
+    }
+}
