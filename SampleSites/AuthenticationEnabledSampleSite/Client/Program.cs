923e5151329513b3de5ba3a14dd0e428175f4083
jsakamoto <junichi.jp@gmail.com>
2020-06-11 18:20:27 +0900

    Add access token based authorization enabled sample site

---
+++ SampleSites/AuthenticationEnabledSampleSite/Client/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace SampleSite.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddHttpClient("SampleSite.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("SampleSite.ServerAPI"));
+
+            builder.Services.AddApiAuthorization();
+
+            builder.Services.AddI18nText();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
