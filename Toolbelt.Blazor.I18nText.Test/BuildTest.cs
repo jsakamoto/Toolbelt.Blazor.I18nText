decd8a33e7552e1fb559f324ae7faeb3138595e1
jsakamoto <junichi.jp@gmail.com>
2020-11-18 20:42:34 +0900

    Improve unit test - change test framework to NUnit for more parallelization.

--- Toolbelt.Blazor.I18nText.Test/BuildTest.cs
+++ Toolbelt.Blazor.I18nText.Test/BuildTest.cs
@@ -3,79 +3,15 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using Xunit;
+using NUnit.Framework;
 using Toolbelt.Blazor.I18nText.Test.Internals;
 using static Toolbelt.Blazor.I18nText.Test.Internals.Shell;
 
 namespace Toolbelt.Blazor.I18nText.Test
 {
+    [Parallelizable(ParallelScope.Children)]
     public class BuildTest
     {
-        private static readonly HashSet<string> DirOfUpdatedProj = new HashSet<string>();
-
-        public BuildTest()
-        {
-            lock (DirOfUpdatedProj)
-            {
-                UpdateLibraryProjects();
-                UpdatePackageRef();
-            }
-        }
-
-        private static bool CheckAndMarkUpdated(string projectDir)
-        {
-            if (DirOfUpdatedProj.Contains(projectDir)) return true;
-            DirOfUpdatedProj.Add(projectDir);
-            return false;
-        }
-
-        private static void UpdateLibraryProjects()
-        {
-            var testDir = WorkSpace.GetTestDir();
-            foreach (var libName in new[] { "Lib4PackRef", "Lib4ProjRef" })
-            {
-                var libDir = Path.Combine(testDir, libName);
-                var libProjPath = Path.Combine(libDir, libName + ".csproj");
-                var libProjDocBefore = XDocument.Load(libProjPath);
-                var i18textVersionBefore = libProjDocBefore.Descendants("PackageReference").Where(node => node.Attribute("Include").Value == "Toolbelt.Blazor.I18nText").First().Attribute("Version").Value;
-
-                if (CheckAndMarkUpdated(libDir)) continue;
-
-                Run(libDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText").ExitCode.Is(0);
-
-                var libProjDocAfter = XDocument.Load(libProjPath);
-                var i18textVersionAfter = libProjDocAfter.Descendants("PackageReference").Where(node => node.Attribute("Include").Value == "Toolbelt.Blazor.I18nText").First().Attribute("Version").Value;
-                if (i18textVersionBefore != i18textVersionAfter)
-                {
-                    var libVerNode = libProjDocAfter.Descendants("Version").First();
-                    var libCurVer = Version.Parse(libVerNode.Value);
-                    var libNextVer = $"{libCurVer.ToString(3)}.{libCurVer.Revision + 1}";
-                    libVerNode.Value = libNextVer;
-                    libProjDocAfter.Save(libProjPath);
-                }
-
-                Run(libDir, "dotnet", "build").ExitCode.Is(0);
-            }
-        }
-
-        private static void UpdatePackageRef()
-        {
-            var testDir = WorkSpace.GetTestDir();
-            var projectNames = new[] { "Components", "Client", "Server" };
-            foreach (var projectName in projectNames)
-            {
-                var projectDir = Path.Combine(testDir, projectName);
-                if (!CheckAndMarkUpdated(projectDir))
-                {
-                    Run(projectDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText").ExitCode.Is(0);
-                    if (projectName == "Components")
-                    {
-                        Run(projectDir, "dotnet", "add", "package", "Lib4PackRef").ExitCode.Is(0);
-                    }
-                }
-            }
-        }
-
         private static string GetPlatform(string startupProjDir)
         {
             var projPath = Directory.GetFiles(startupProjDir, "*.csproj").First();
@@ -89,8 +25,8 @@ namespace Toolbelt.Blazor.I18nText.Test
             new object[]{ "Server" },
         };
 
-        [Theory(DisplayName = "Build")]
-        [MemberData(nameof(Projects))]
+        [Test]
+        [TestCaseSource(nameof(Projects))]
         public void BasicBuildTest(string startupProjName)
         {
             using var workSpace = WorkSpace.Create(startupProjName);
@@ -113,8 +49,8 @@ namespace Toolbelt.Blazor.I18nText.Test
             );
         }
 
-        [Theory(DisplayName = "Publish")]
-        [MemberData(nameof(Projects))]
+        [Test]
+        [TestCaseSource(nameof(Projects))]
         public void PublishTest(string startupProjName)
         {
             using var workSpace = WorkSpace.Create(startupProjName);
---
+++ Toolbelt.Blazor.I18nText.Test/SetUpFixture.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Toolbelt.Blazor.I18nText.Test.Internals;
+using static Toolbelt.Blazor.I18nText.Test.Internals.Shell;
+
+namespace Toolbelt.Blazor.I18nText.Test
+{
+    [SetUpFixture]
+    public class SetUpFixture
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            UpdateLibraryProjects();
+            UpdatePackageRef();
+        }
+
+        private static void UpdateLibraryProjects()
+        {
+            var testDir = WorkSpace.GetTestDir();
+            foreach (var libName in new[] { "Lib4PackRef", "Lib4ProjRef" })
+            {
+                var libDir = Path.Combine(testDir, libName);
+                var libProjPath = Path.Combine(libDir, libName + ".csproj");
+                var libProjDocBefore = XDocument.Load(libProjPath);
+                var i18textVersionBefore = libProjDocBefore.Descendants("PackageReference").Where(node => node.Attribute("Include").Value == "Toolbelt.Blazor.I18nText").First().Attribute("Version").Value;
+
+                Run(libDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText").ExitCode.Is(0);
+
+                var libProjDocAfter = XDocument.Load(libProjPath);
+                var i18textVersionAfter = libProjDocAfter.Descendants("PackageReference").Where(node => node.Attribute("Include").Value == "Toolbelt.Blazor.I18nText").First().Attribute("Version").Value;
+                if (i18textVersionBefore != i18textVersionAfter)
+                {
+                    var libVerNode = libProjDocAfter.Descendants("Version").First();
+                    var libCurVer = Version.Parse(libVerNode.Value);
+                    var libNextVer = $"{libCurVer.ToString(3)}.{libCurVer.Revision + 1}";
+                    libVerNode.Value = libNextVer;
+                    libProjDocAfter.Save(libProjPath);
+                }
+
+                Run(libDir, "dotnet", "build").ExitCode.Is(0);
+            }
+        }
+
+        private static void UpdatePackageRef()
+        {
+            var testDir = WorkSpace.GetTestDir();
+            var projectNames = new[] { "Components", "Client", "Server" };
+            foreach (var projectName in projectNames)
+            {
+                var projectDir = Path.Combine(testDir, projectName);
+                Run(projectDir, "dotnet", "add", "package", "Toolbelt.Blazor.I18nText").ExitCode.Is(0);
+                if (projectName == "Components")
+                {
+                    Run(projectDir, "dotnet", "add", "package", "Lib4PackRef").ExitCode.Is(0);
+                }
+            }
+        }
+    }
+}
