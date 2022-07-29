3a0ccc817b865ab76cce8f0f46aa22faccf42d1e
jsakamoto <junichi.jp@gmail.com>
2022-07-29 22:14:28 +0900

    Improve performance of I18nText late binding

--- Toolbelt.Blazor.I18nText/I18nTextExtensions.cs
+++ Toolbelt.Blazor.I18nText/I18nTextExtensions.cs
@@ -1,10 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using Toolbelt.Blazor.I18nText.Interfaces;
 
 namespace Toolbelt.Blazor.I18nText
 {
     public static class I18nTextExtensions
     {
+        private static class FieldsCache<T>
+        {
+            public static readonly IReadOnlyDictionary<string, FieldInfo> Fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Instance).ToDictionary(f => f.Name, f => f);
+        }
+
         public static string? GetFieldValue<T>(this T value, string? key) where T : I18nTextLateBinding
         {
             return value.GetFieldValueWithNoFallback(key) ?? key;
@@ -13,7 +20,7 @@ namespace Toolbelt.Blazor.I18nText
         public static string? GetFieldValueWithNoFallback<T>(this T value, string? key) where T : I18nTextLateBinding
         {
             if (string.IsNullOrEmpty(key)) return "";
-            return value.GetType().GetField(key, BindingFlags.Public | BindingFlags.Instance)?.GetValue(value) as string;
+            return FieldsCache<T>.Fields.TryGetValue(key!, out var field) ? field.GetValue(value) as string : null;
         }
     }
 }
