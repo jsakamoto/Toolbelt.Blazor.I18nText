5fba226a9f3e93ece604ddfc5850770d75087007
jsakamoto <junichi.jp@gmail.com>
2024-08-03 21:36:36 +0900

    v.13.0.0 release

--- Toolbelt.Blazor.I18nText/VersionInfo.cs
+++ Toolbelt.Blazor.I18nText/VersionInfo.cs
@@ -1,5 +1,5 @@
 namespace Toolbelt.Blazor.I18nText;
 internal static class VersionInfo
 {
-    internal const string VersionText = "13.0.0-preview.2";
+    internal const string VersionText = "13.0.0";
 }
