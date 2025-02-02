f5de98475e7beddd51c74fdba3320f712cacb7f6
jsakamoto <junichi.jp@gmail.com>
2025-02-02 21:01:29 +0900

    Update the package version number

--- Toolbelt.Blazor.I18nText/VersionInfo.cs
+++ Toolbelt.Blazor.I18nText/VersionInfo.cs
@@ -1,5 +1,5 @@
 namespace Toolbelt.Blazor.I18nText;
 internal static class VersionInfo
 {
-    internal const string VersionText = "14.0.0-preview.3";
+    internal const string VersionText = "14.0.0-preview.4";
 }
