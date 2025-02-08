48913911de164253abe46b380d411d7881d4d059
jsakamoto <junichi.jp@gmail.com>
2025-02-08 22:56:21 +0900

    v.14.0.0 release

--- Toolbelt.Blazor.I18nText/VersionInfo.cs
+++ Toolbelt.Blazor.I18nText/VersionInfo.cs
@@ -1,6 +1,6 @@
 namespace Toolbelt.Blazor.I18nText;
 internal static class VersionInfo
 {
-    internal const string VersionText = "14.0.0-preview.5";
+    internal const string VersionText = "14.0.0";
     internal static bool IsOptimizedForWasm() => false;
 }
