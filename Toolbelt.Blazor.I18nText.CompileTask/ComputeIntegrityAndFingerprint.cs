67682f54ef867edbee6973a7a8e3cda77c556aee
jsakamoto <junichi.jp@gmail.com>
2025-05-22 16:38:53 +0900

    Add support .NET SDK 9.0.300
    - Fix the following build error:
      - 'File length for "<path>" is not defined'
      - 'LastWriteTime for "<path>" is not defined'
      - 'Endpoints not found for related asset: "<path>"'

--- Toolbelt.Blazor.I18nText.CompileTask/ComputeIntegrityAndFingerprint.cs
+++ Toolbelt.Blazor.I18nText.CompileTask/ComputeIntegrityAndFingerprint.cs
@@ -28,22 +28,31 @@ namespace Toolbelt.Blazor.I18nText.CompileTask
             using var sha256 = SHA256.Create();
             foreach (var item in this.Items)
             {
-                var hash = ComputeHash(sha256, item);
+                var path = GetFilePath(item);
+                var fileInfo = new FileInfo(path);
+                var hash = ComputeHash(sha256, path);
                 item.SetMetadata("Integrity", Convert.ToBase64String(hash));
                 item.SetMetadata("Fingerprint", Base36.Encode(hash));
+                item.SetMetadata("FileLength", fileInfo.Length.ToString());
+                item.SetMetadata("LastWriteTime", fileInfo.LastWriteTimeUtc.ToString("R")); // ex."Tue, 20 May 2025 23:21:08 GMT"
             }
 
             this.ComputedItems = this.Items;
             return true;
         }
 
-        private static byte[] ComputeHash(SHA256 sha256, ITaskItem item)
+        private static byte[] ComputeHash(SHA256 sha256, string path)
+        {
+            var contentBytes = File.ReadAllBytes(path);
+            return sha256.ComputeHash(contentBytes);
+        }
+
+        private static string GetFilePath(ITaskItem item)
         {
             var path = item.GetMetadata("OriginalItemSpec");
             if (!File.Exists(path)) path = item.GetMetadata("FullPath");
             if (!File.Exists(path)) path = item.GetMetadata("Identity");
-            var contentBytes = File.ReadAllBytes(path);
-            return sha256.ComputeHash(contentBytes);
+            return path;
         }
     }
 }
