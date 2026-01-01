c811c452118e7d759e3407a4dc582920e7155561
jsakamoto <junichi.jp@gmail.com>
2026-01-01 14:45:04 +0900

    Fix the package dependency and repack

--- Toolbelt.Blazor.I18nText/VersionInfo.cs
+++ Toolbelt.Blazor.I18nText/VersionInfo.cs
@@ -1,6 +1,6 @@
 namespace Toolbelt.Blazor.I18nText;
 internal static class VersionInfo
 {
-    internal const string VersionText = "14.1.0";
+    internal const string VersionText = "14.1.1";
     internal static bool IsOptimizedForWasm() => false;
 }
